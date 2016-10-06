0f58eb5b7f40afc7af27790f18a2afe53f9b2fa0
wmcb91 <wmcb91@gmail.com>
2016-10-06 16:08:36 -0400

    Delete old comments from index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,14 +8,10 @@ require('./example');
 
 const authEvents = require('./auth/events');
 const booksEvents = require('./books/events');
-// const render = require('./app/render');
-// const app = require('./app');
 
 
 // On document ready
 $(() => {
-  // navEvents.addHandlers();
-  // render(app);
   authEvents.addHandlers();
   booksEvents.addHandlers();
 
