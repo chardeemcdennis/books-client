460a6e469aa3f9e9ea80035c292c89d9528e1602
wmcb91 <wmcb91@gmail.com>
2016-10-06 16:08:04 -0400

    Add handlers to index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,18 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+const authEvents = require('./auth/events');
+const booksEvents = require('./books/events');
+// const render = require('./app/render');
+// const app = require('./app');
+
+
+// On document ready
+$(() => {
+  // navEvents.addHandlers();
+  // render(app);
+  authEvents.addHandlers();
+  booksEvents.addHandlers();
+
+});
